c4a91e599d90790a2701681c1ca1ab6dee86f3a3
barothavinash1 <avinash.baroth@gmail.com>
2025-09-06 12:33:47 -0400

    demoing how to use gitignore

--- PoC.py
+++ PoC.py
@@ -1,14 +1,27 @@
-#variable declaration 
-name = "test" #string datatype always in qutotes 
-# number_of_run = 10 #integer 
-# number_of_runs = "10" #string
-# number_of_reruns = "10.1" #floating
-# success = False #boolen
-
-print ("name")
-
-# variables
-
-number = 10
-
-print ("number")
+first_name = "avinash"
+last_name = "baroth"
+skill_level = 8
+target_skill_level = 10
+rank = 1.0
+print (first_name)
+diff_skill_level = target_skill_level - skill_level
+print (diff_skill_level)
+type (diff_skill_level)
+type (first_name)
+print (first_name + " " + last_name)
+print (first_name + " " + last_name + " - " + str(skill_level))
+print (target_skill_level + rank)
+float_num = 10.7
+int_num = int(float_num)
+print(f"Float to int: {int_num}, type: {type(int_num)}")
+truthy_val = bool(1)
+falsy_val = bool(0)
+empty_string_bool = bool("")
+non_empty_string_bool = bool("hello")
+print(f"1 to bool: {truthy_val}")
+print(f"0 to bool: {falsy_val}")
+print(f"Empty string to bool: {empty_string_bool}")
+print(f"Non-empty string to bool: {non_empty_string_bool}")
+int_val = 20
+float_val = float(int_val)
+print(f"Int to float: {float_val}, type: {type(float_val)}")
